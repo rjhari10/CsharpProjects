b9b68f16e33ec9897dc0a55d6340113bbcfe2066
RJ Hari <richardjhari@outlook.com>
2024-09-06 21:46:31 -0400

    update TestProjects for int array exercise

--- TestProjects/Program.cs
+++ TestProjects/Program.cs
@@ -1,27 +1,10 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
-
-if (daysUntilExpiration == 0)
+﻿int[] inventory = { 200, 450, 700, 175, 250};
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
 {
-    Console.WriteLine("Your subscription has expired.");
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
 }
-else if (daysUntilExpiration == 1)
-{
-    Console.WriteLine("Your subscription will expire within a day!");
-    discountPercentage = 20;
-}
-else if (daysUntilExpiration <= 5)
-{
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
-    discountPercentage = 10;
-}
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
-if (discountPercentage > 0)
-{
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
-}
+Console.WriteLine($"We have {sum} items in inventory.");
